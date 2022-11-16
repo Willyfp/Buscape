6625f6cd110fe011da5f868e800e554dd40f20aa
willy <willypereira41@gmail.com>
2022-11-16 20:43:41 -0300

    Correção erro nos favoritos

--- src/pages/Profile/components/Favorites/index.tsx
+++ src/pages/Profile/components/Favorites/index.tsx
@@ -1,12 +1,12 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { Fragment, useState } from 'react';
 import { Appbar, Divider, List } from 'react-native-paper';
+import { Image } from 'react-native-ui-lib';
 import { connect, ConnectedProps } from 'react-redux';
 import { useTheme } from 'styled-components';
 import { RootState } from '../../../../store/reducers';
-import { Image } from 'react-native-ui-lib';
-import ModalApt from '../../../Home/components/ModalApt';
 import { GetAppartmentsCreators } from '../../../../store/reducers/appartments';
+import ModalApt from '../../../Home/components/ModalApt';
 
 const mapStateToProps = ({ auth, appartments }: RootState) => ({
   user: auth.getIn(['user']),
@@ -66,7 +66,7 @@ const Favorites = ({
                   style={{ width: 100, height: 100 }}
                   source={{
                     uri:
-                      apart.images?.[0].uri ||
+                      apart.images?.[0]?.uri ||
                       'https://th.bing.com/th/id/OIP.hV6MoBaE8NYeMCugmhd7_QHaEo?w=288&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7',
                   }}
                 />
